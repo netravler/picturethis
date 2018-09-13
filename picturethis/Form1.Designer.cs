ce55ef39248d21f347078320d476ad6a4d78c62e
netravler <pheintz@hotmail.com>
2018-09-13 11:48:13 -0500

    Add files via upload

---
+++ picturethis/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace picturethis
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnInsertImage = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.btnPrevious = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.pictureBox1.Location = new System.Drawing.Point(36, 38);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(866, 584);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnInsertImage
+            // 
+            this.btnInsertImage.Location = new System.Drawing.Point(923, 39);
+            this.btnInsertImage.Name = "btnInsertImage";
+            this.btnInsertImage.Size = new System.Drawing.Size(75, 31);
+            this.btnInsertImage.TabIndex = 1;
+            this.btnInsertImage.Text = "Insert Image";
+            this.btnInsertImage.UseVisualStyleBackColor = true;
+            this.btnInsertImage.Click += new System.EventHandler(this.btnInsertImage_Click);
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(923, 68);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(75, 31);
+            this.btnNext.TabIndex = 2;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // btnPrevious
+            // 
+            this.btnPrevious.Location = new System.Drawing.Point(923, 97);
+            this.btnPrevious.Name = "btnPrevious";
+            this.btnPrevious.Size = new System.Drawing.Size(75, 31);
+            this.btnPrevious.TabIndex = 3;
+            this.btnPrevious.Text = "Previous";
+            this.btnPrevious.UseVisualStyleBackColor = true;
+            this.btnPrevious.Click += new System.EventHandler(this.btnPrevious_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1020, 645);
+            this.Controls.Add(this.btnPrevious);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnInsertImage);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Picture Box";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnInsertImage;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnPrevious;
+    }
+}
+
---
+++ picturethis/Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace picturethis
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection con = new SqlConnection("data source=localhost;uid=sa;password=;database=picturethis");
+        SqlCommand com;
+        SqlDataAdapter ad;
+        FileStream fs;
+        byte[] rawdata;
+        DataSet ds = new DataSet();
+        int i = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnInsertImage_Click(object sender, EventArgs e)
+        {
+            //To show the File Dialog to get the path of the image
+
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.ShowDialog();
+
+            fs = new FileStream(openFileDialog1.FileName, FileMode.Open, FileAccess.Read);
+
+            rawdata = new byte[Convert.ToInt32(fs.Length)];
+
+            fs.Read(rawdata, 0, Convert.ToInt32(fs.Length));
+
+            con.Open();
+
+            com = new SqlCommand("INSERT INTO picturethis(Picture) VALUES(@k)", con);
+
+            com.Parameters.AddWithValue("@k", rawdata);
+
+            com.ExecuteNonQuery();
+
+            con.Close();
+
+            pictureBox1.Image = Image.FromStream(new MemoryStream((byte[])rawdata));
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            con.Open();
+
+            ad = new SqlDataAdapter("SELECT Picture FROM Picturethis", con);
+
+            ad.Fill(ds);
+
+            con.Close();
+
+            int k = ds.Tables[0].Rows.Count;
+
+            i++;
+
+            if (i < k)
+
+                pictureBox1.Image = Image.FromStream(new MemoryStream((byte[])ds.Tables[0].Rows[i].ItemArray[0]));
+
+            else
+            {
+
+                i = 0;
+
+                pictureBox1.Image = Image.FromStream(new MemoryStream((byte[])ds.Tables[0].Rows[i].ItemArray[0]));
+
+            }
+        }
+
+        private void btnPrevious_Click(object sender, EventArgs e)
+        {
+            con.Open();
+
+            ad = new SqlDataAdapter("SELECT Picture FROM Picturethis", con);
+
+            ad.Fill(ds);
+
+            con.Close();
+
+            int k = ds.Tables[0].Rows.Count;
+
+            i--;
+
+            if (i >= 0)
+
+                pictureBox1.Image = Image.FromStream(new MemoryStream((byte[])ds.Tables[0].Rows[i].ItemArray[0]));
+
+            else
+            {
+
+                i = k;
+
+                
+                
+                
+                
+                pictureBox1.Image = Image.FromStream(new MemoryStream((byte[])ds.Tables[0].Rows[i].ItemArray[0]));
+
+            }
+        }
+    }
+}
